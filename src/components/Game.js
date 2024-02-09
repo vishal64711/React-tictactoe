8f4debc6ace5556459a924ae90a845a84e01637a
Vishal <vishal64711@gmail.com>
2024-02-09 21:35:04 +0530

    project complete

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,7 +4,7 @@ import Board from './Board'
 export default class Game extends React.Component{
 
     constructor(props){
-        super()
+        super(props)
         this.state = {
             xIsNext: true,
             stepNumber: 0,
@@ -18,6 +18,12 @@ export default class Game extends React.Component{
         const history = this.state.history
         const current = history[history.length -1]
         const squares = current.squares
+
+        const winner = calculateWinners(squares)
+
+        if(winner || squares[i]){
+            return 
+        }
 
         squares[i] = this.state.xIsNext ? 'X' : 'O'
         this.setState({
@@ -33,12 +39,49 @@ export default class Game extends React.Component{
         const history = this.state.history
         const current = history[this.state.stepNumber]
 
+        let status 
+        const winner = calculateWinners(current.squares)
+        if(winner){
+            status = "Winner is " + winner
+        }
+        else if(this.state.stepNumber ===9){ status= "Tie"}
+        else{
+            status = 'Next Player is ' + (this.state.xIsNext ? 'X' : 'O')
+        }
+
         return(
             <div className="game">
                 <div className= "game-board">
                     <Board clickAction = {(i)=>this.handleClick(i)} squares = {current.squares}/>
                 </div>
+
+                <div className='game-info'>
+                    <div> {status}</div>
+                </div>
+
             </div>
         )
     }
+}
+
+function calculateWinners(squares){
+    const possibilities = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    for(let i=0; i<possibilities.length; i++){
+        const[a,b,c] = possibilities[i]
+        if(squares[a] && squares[a]=== squares[b]&& squares[b]===squares[c]){
+            return squares[a]
+        }
+    }
+    return null
+
 }
